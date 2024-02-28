052affe3e3e72c48f7c59222e24d6c117b8e5293
PauloGN <paulorsantos.gnt@outlook.com>
2024-02-27 16:15:44 -0800

    1 -  Project Creation

---
+++ Source/ChronoQuest/ChronoQuest.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ChronoQuest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ChronoQuest, "ChronoQuest" );
+ 
---
+++ Source/ChronoQuest/ChronoQuestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChronoQuestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AChronoQuestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AChronoQuestGameMode();
+};
+
+
+
