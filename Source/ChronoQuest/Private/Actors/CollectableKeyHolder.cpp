1f8e66eac788ca60e4497767eab22fc52120522a
PauloGN <paulorsantos.gnt@outlook.com>
2024-03-22 12:07:47 -0700

    Implementing a key holder add cpp class for keyholder add blueprint actor key holder

---
+++ Source/ChronoQuest/Private/Actors/CollectableKeyHolder.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/CollectableKeyHolder.h"
+
+// Sets default values
+ACollectableKeyHolder::ACollectableKeyHolder()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bIsKeyCollected = false;
+	KeyRotationSpeed = 0.0f;
+	bReplicates = true;
+    AActor::SetReplicateMovement(true);
+
+	rootComp = CreateDefaultSubobject<USceneComponent>(TEXT("Root Comp"));
+	SetRootComponent(rootComp);
+
+	holderMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Holder Mesh"));
+	holderMesh->SetupAttachment(rootComp);
+	holderMesh->SetIsReplicated(true);
+	holderMesh->SetCollisionProfileName("BlockAllDynamic");
+	keyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Key Mesh"));
+	keyMesh->SetupAttachment(rootComp);
+	keyMesh->SetIsReplicated(true);
+	keyMesh->SetCollisionProfileName("OverlapAllDynamic");
+}
+
+void ACollectableKeyHolder::ActivateKeyMesh()
+{
+	keyMesh->SetVisibility(true);
+	bIsKeyCollected = true;
+}
+
+// Called when the game starts or when spawned
+void ACollectableKeyHolder::BeginPlay()
+{
+	Super::BeginPlay();
+
+	keyMesh->SetVisibility(false);
+
+}
+
+// Called every frame
+void ACollectableKeyHolder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!bIsKeyCollected) return;
+	keyMesh->SetRelativeRotation(FRotator(0.0f, KeyRotationSpeed * DeltaTime, 0.0f));
+}
+
---
+++ Source/ChronoQuest/Private/Actors/CollectableKeyHolder.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CollectableKeyHolder.generated.h"
+
+UCLASS()
+class ACollectableKeyHolder : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACollectableKeyHolder();
+
+	UFUNCTION()
+	void ActivateKeyMesh();
+
+	bool bIsKeyCollected;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	USceneComponent* rootComp;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UStaticMeshComponent* holderMesh;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UStaticMeshComponent* keyMesh;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	float KeyRotationSpeed;
+};
