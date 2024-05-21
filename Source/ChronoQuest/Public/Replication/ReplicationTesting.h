445445f4c1ce4d42c6f1d33186089e3b597169ec
PauloGN <paulorsantos.gnt@outlook.com>
2024-05-21 13:52:42 -0700

    Walk run test -  Fix: marking rep props as virtual function

--- Source/ChronoQuest/Public/Replication/ReplicationTesting.h
+++ Source/ChronoQuest/Public/Replication/ReplicationTesting.h
@@ -15,7 +15,7 @@ public:
 	// Sets default values for this actor's properties
 	AReplicationTesting();
 
-	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 protected:
 	// Called when the game starts or when spawned
